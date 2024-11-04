036c50a3d8c10577b7daef1c69edfba69d5cbe3b
bemuse01 <sequeland@gmail.com>
2024-11-05 00:20:32 +0900

    updaste

---
+++ dockerfile
@@ -0,0 +1,42 @@
+# FROM ubuntu:20.04
+
+# WORKDIR /app
+
+# RUN apt-get update && apt-get install -y \
+#     python3 \
+#     python3-pip \
+#     nodejs \
+#     npm \
+#     && apt-get clean \
+#     && rm -rf /var/lib/apt/lists/*
+
+# RUN pip3 install --no-cache-dir yt-dlp
+
+# COPY package*.json ./
+
+# RUN npm install
+
+# COPY . .
+
+# CMD ["node", "src/utils/videoProcessor.js"]
+
+FROM python:3.11-slim
+
+RUN apt-get update && apt-get install -y \
+    curl \
+    ffmpeg \
+    && curl -fsSL https://deb.nodesource.com/setup_18.x | bash - \
+    && apt-get install -y nodejs \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+
+WORKDIR /app
+
+RUN pip3 install --no-cache-dir yt-dlp
+
+COPY package*.json ./
+RUN npm install
+
+COPY . .
+
+CMD ["node", "src/utils/videoProcessor.js"]
--- src/utils/videoProcessor.js
+++ src/utils/videoProcessor.js
@@ -1,6 +1,38 @@
 import { AUDIO_SAVE_PATH } from '../config/urls.js'
+import { exec } from 'child_process'
 
+
+const download = (cmd) => {
+    return new Promise((resolve, reject) => {
+        exec(cmd, (err, stdout, stderr) => {
+
+            if(err){
+                console.log(err.message)
+                reject(err)
+                return
+            }
+
+            if(stderr){
+                console.log(stderr)
+                reject(stderr)
+                return
+            }
+
+            resolve()
+            console.log(stdout)
+
+        })
+    })
+}
 
 const processVideo = async () => {
-    
+
+    const url = 'https://www.youtube.com/watch?v=PiIAVnFX2eo'
+    const id = 'PiIAVnFX2eo'
+    const command = `yt-dlp --extract-audio --audio-format mp3 -o "${AUDIO_SAVE_PATH}${id}.%(ext)s" ${url}`
+
+    await download(command)
+
 }
+
+processVideo()
