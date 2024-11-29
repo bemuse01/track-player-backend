a2a33d510064364fd7498120507a993a396ce204
bemuse01 <sequeland@gmail.com>
2024-11-29 21:04:32 +0900

    Update cors.js

--- src/plugins/cors.js
+++ src/plugins/cors.js
@@ -1,6 +1,8 @@
 import fastifyCors from '@fastify/cors'
 import 'dotenv/config'
 
+// TODO origin will be nginx domain. this will be changed later like this https://nginx-domain.com
+
 const corsOption = {
     origin: process.env.CLIENT_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
